5217f8c87a5085648a4c68ec055fe3443102ef3a
Zygimantas <zygimantas.bogusevicius@gmail.com>
2021-12-01 00:11:54 +0200

    Additional Tasks

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -73,65 +73,209 @@ namespace Homework
             string word = Convert.ToString(wordNumber);
 
             string numberInText = "";
-            string individualNumber;
-
-            for (int i = 0; i < word.Length; i++)
+            string individualNumber= "";
+            int symbolCount = word.Length;
+            int firstSymbol = (int)word[0];
+            bool negativeNumber = false;
+            if (firstSymbol == 45)
             {
-                char symbol = word[i];
+                negativeNumber = true;
+                symbolCount--;
+            }
 
-                if (i == 0 & (int)symbol == 45)
+            if (symbolCount == 1)
+            {
+                if (negativeNumber)
                 {
                     numberInText = "Minus ";
+                    firstSymbol = (int)word[1];
                 }
                 else
                 {
-                    switch ((int)symbol)
+                    firstSymbol = (int)word[0];
+                }
+                numberInText = numberInText + Ones(firstSymbol);
+            }
+
+            else if (symbolCount == 2)
+            {
+                int secondSymbol;
+                if (negativeNumber)
+                {
+                    numberInText = "Minus ";
+                    firstSymbol = (int)word[1];
+                    secondSymbol = (int)word[2];
+                }
+                else
+                {
+                    firstSymbol = (int)word[0];
+                    secondSymbol = (int)word[1];
+                }
+                if (firstSymbol == 49)
+                {
+                    numberInText = numberInText + individualNumber + DesimtDevyniolika(secondSymbol);
+                }
+                else
+                {
+                    numberInText = numberInText + Tens(firstSymbol) + "desimt " + Ones(secondSymbol);
+                }
+            }
+            else if (symbolCount == 3)
+            {
+                int secondSymbol;
+                int thirdSymbol;
+                if (negativeNumber)
+                {
+                    numberInText = "Minus ";
+                    firstSymbol = (int)word[1];
+                    secondSymbol = (int)word[2];
+                    thirdSymbol = (int)word[3];
+                }
+                else
+                {
+                    firstSymbol = (int)word[0];
+                    secondSymbol = (int)word[1];
+                    thirdSymbol = (int)word[2];
+                }
+                if (firstSymbol == 49)
+                {
+                    if (secondSymbol == 49)
                     {
-                        case 48:
-                            individualNumber = "Nulis";
-                            numberInText = numberInText + individualNumber;
-                            break;
-                        case 49:
-                            individualNumber = "Vienas";
-                            numberInText = numberInText + individualNumber;
-                            break;
-                        case 50:
-                            individualNumber = "Du";
-                            numberInText = numberInText + individualNumber;
-                            break;
-                        case 51:
-                            individualNumber = "Trys";
-                            numberInText = numberInText + individualNumber;
-                            break;
-                        case 52:
-                            individualNumber = "Keturi";
-                            numberInText = numberInText + individualNumber;
-                            break;
-                        case 53:
-                            individualNumber = "Penki";
-                            numberInText = numberInText + individualNumber;
-                            break;
-                        case 54:
-                            individualNumber = "Sesi";
-                            numberInText = numberInText + individualNumber;
-                            break;
-                        case 55:
-                            individualNumber = "Septyni";
-                            numberInText = numberInText + individualNumber;
-                            break;
-                        case 56:
-                            individualNumber = "Astuoni";
-                            numberInText = numberInText + individualNumber;
-                            break;
-                        case 57:
-                            individualNumber = "Devyni";
-                            numberInText = numberInText + individualNumber;
-                            break;
+                        numberInText = numberInText + Ones(firstSymbol) + " simtas " + DesimtDevyniolika(thirdSymbol);
+                    }
+                    else
+                    {
+                        numberInText = numberInText + Ones(firstSymbol) + " simtas " + Tens(secondSymbol) + "desimt " + Ones(thirdSymbol);
+                    }
+                }
+                else
+                {
+                    if (secondSymbol == 49)
+                    {
+                        numberInText = numberInText + Ones(firstSymbol) + " simtai " + DesimtDevyniolika(thirdSymbol);
+                    }
+                    else
+                    {
+                        numberInText = numberInText + Ones(firstSymbol) + " simtai " + Tens(secondSymbol) + "desimt " + Ones(thirdSymbol);
                     }
                 }
             }
+            else
+            {
+                numberInText = "Error";
+            }
             return numberInText;
         }
+
+        static string Ones(int Symbol)
+        {
+            string individualNumber = "";
+            switch (Symbol)
+            {
+                case 48:
+                    individualNumber = "";
+                    break;
+                case 49:
+                    individualNumber = "vienas";
+                    break;
+                case 50:
+                    individualNumber = "du";
+                    break;
+                case 51:
+                    individualNumber = "trys";
+                    break;
+                case 52:
+                    individualNumber = "keturi";
+                    break;
+                case 53:
+                    individualNumber = "penki";
+                    break;
+                case 54:
+                    individualNumber = "sesi";
+                    break;
+                case 55:
+                    individualNumber = "septyni";
+                    break;
+                case 56:
+                    individualNumber = "astuoni";
+                    break;
+                case 57:
+                    individualNumber = "devyni";
+                    break;
+            }
+            return individualNumber;
+        }
+        static string Tens(int Symbol)
+        {
+            string tens = "";
+            switch (Symbol)
+            {
+                case 50:
+                    tens = "Dvi";
+                    break;
+                case 51:
+                    tens = "Tris";
+                    break;
+                case 52:
+                    tens = "Keturias";
+                    break;
+                case 53:
+                    tens = "Penkias";
+                    break;
+                case 54:
+                    tens = "Sesias";
+                    break;
+                case 55:
+                    tens = "Septynias";
+                    break;
+                case 56:
+                    tens = "Astuonias";
+                    break;
+                case 57:
+                    tens = "Devynias";
+                    break;
+            }
+            return tens;
+        }
+        static string DesimtDevyniolika(int Symbol)
+        {
+            string individualNumber = "";
+            switch (Symbol)
+            {
+                case 48:
+                    individualNumber = "Desimt";
+                    break;
+                case 49:
+                    individualNumber = "Vienuolika";
+                    break;
+                case 50:
+                    individualNumber = "Dvylika";
+                    break;
+                case 51:
+                    individualNumber = "Trylika";
+                    break;
+                case 52:
+                    individualNumber = "Keturiolika";
+                    break;
+                case 53:
+                    individualNumber = "Penkiolika";
+                    break;
+                case 54:
+                    individualNumber = "Sesiolika";
+                    break;
+                case 55:
+                    individualNumber = "Septyniolika";
+                    break;
+                case 56:
+                    individualNumber = "Astuoniolika";
+                    break;
+                case 57:
+                    individualNumber = "Devyniolika";
+                    break;
+            }
+            return individualNumber;
+        }
+
     }
 }
 
